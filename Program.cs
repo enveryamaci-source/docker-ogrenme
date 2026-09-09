0b3f9b23e79f7aa76a7e720a6791ae931379d00d
enveryamaci-source <enveryamaci@gmail.com>
2026-09-09 12:38:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Sigma kuralları!!!!!!");
+Console.WriteLine("Matrix'ten sinyal: Birisi siteye baglandi!");
 
 app.Run();
