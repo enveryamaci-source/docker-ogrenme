7f48dc5b5c19f020078bfd1f7561871ba1fdfc6e
enveryamaci-source <enveryamaci@gmail.com>
2026-09-11 16:40:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+string dbAdresi = "Host=veritabani;Port=5432;Database=oyun_verileri;Username=admin;Password=gizlisifre123";
+Console.WriteLine($"[SİSTEM BAŞLATILDI] Veritabanı hedefi: {dbAdresi}");
+
 app.MapGet("/", () => {
     Console.WriteLine("Matrix'ten sinyal: Birisi siteye baglandi!");
     return "Hello Sigma";
